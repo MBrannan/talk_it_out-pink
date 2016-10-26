c221f636a54a76a7a6e9751c34cfb057f374f40f
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-10-26 11:04:05 -0500

    Add js code and stylesheet

--- peer_JS2_01.js
+++ peer_JS2_01.js
@@ -1,3 +1,61 @@
+var value = 42;
+
+if (value >= 53) {
+  value += 42;
+} else {
+  value -= 13;
+}
+
+value = value + "11";
+
+var array = [];
+  for (var i = 0; i < value.length; i++) {
+    array.push(value.charAt(i));
+  }
+
+array.pop();
+
+array.shift();
+
+var newVar = "";
+
+for (i = array.length - 1; i >= 0; i--) {
+  newVar += array[i];
+}
+
+value = parseInt(value);
+
+newVar = parseInt(newVar);
+
+value += newVar;
+
+if (value < 60) {
+  value = 14;
+} else if (value == 2930){
+    value = 27;
+} else {
+  value = 2;
+}
+
+i = 10;
+while (i > 0) {
+  value++;
+  i--;
+}
+
+function functionA(val) {
+  val = val.toString();
+  if (val.length > 1) {
+    val = val.substring(1, val.length);
+  }
+  return val;
+}
+
+value = functionA(value);
+
+console.log(value);
+
+
 /*
 1. Start with the number 42 and set that equal to 'value'
 
@@ -19,12 +77,12 @@
 
 8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
 
-9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27. 
+9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27.
 If neither of these are true, set the value to 2.
 
 10. Create a while loop that counts down from 10 and increments 'value' by 1.
 
-11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there 
+11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there
 is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
 
 12. Call the function.
